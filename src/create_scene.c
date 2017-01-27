eb1829b46e78610951d28a34002087b9f335374b
Barbare <michael.barbarin@gmail.com>
2017-01-27 17:32:32 +0100

    create scene change

--- src/create_scene.c
+++ src/create_scene.c
@@ -6,11 +6,30 @@
 /*   By: mbarbari <mbarbari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/26 17:01:53 by mbarbari          #+#    #+#             */
-/*   Updated: 2017/01/27 15:56:40 by mbarbari         ###   ########.fr       */
+/*   Updated: 2017/01/27 17:29:13 by mbarbari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <rt.h>
+
+typedef struct s_objprim t_objprim;
+typedef struct e_typeargs t_typeargs;
+
+enum			e_typeargs
+{
+	args0 = 0x00,
+	args1 = 0x01,
+	args2 = 0x02,
+	args3 = 0x03,
+	args4 = 0x04
+};
+
+struct			s_objprim
+{
+	void		*fct;
+	e_typeargs	args;
+};
+
 
 unsigned long		djb2(const char *str)
 {
@@ -41,6 +60,20 @@ int					get_idfct(unsigned long *key, unsigned long val)
 	return (-1);
 }
 
+void			set_fct_primitive(val, t_objprim prim)
+{
+	if (prim.args == args1)
+		return (prim.fct(json_get(val.data.obj, "radius").data.number));
+	else if (prim.args == args2)
+		return (prim.fct(json_get(val.data.obj, "radius").data.number,
+					json_get(val.data.obj, "size").data.number));
+	else if (prim.args == args4)
+		return (prim.fct(	get_vec3_json(json_get(val.data.obj, "a").data.number),
+							get_vec3_json(json_get(val.data.obj, "b").data.number),
+							get_vec3_json(json_get(val.data.obj, "c").data.number));
+	return (prim.fct());
+}
+
 int				key_primitive(unsigned long id)
 {
 	static unsigned long	key_primitive[20];
@@ -53,57 +86,66 @@ int				key_primitive(unsigned long id)
 		key_primitive[2] = djb2("CYLINDER");
 		key_primitive[3] = djb2("CONE");
 		key_primitive[4] = djb2("TRIANGLE");
+		key_primitive[5] = djb2("DISC");
+		key_primitive[6] = djb2("CAPPED_CYLINDER");
 		key_primitive[5] = 0;
 		++isinit;
 	}
 	return (id == 0 ? -1 : get_idfct(key_primitive, id));
 }
 
-void			*fct_prim(int id)
+t_objprim			fct_prim(int id)
 {
-	static void	*fct_primitive[20];
-	static int	isinit = 0;
+	static t_objprim	fct_primitive[20];
+	static int			isinit = 0;
 
 	if (isinit == 0)
 	{
-		fct_primitive[0] = &new_sphere;
-		fct_primitive[1] = &new_plane;
-		fct_primitive[2] = &new_cylinder;
-		fct_primitive[3] = &new_cone;
-		fct_primitive[4] = &new_triangle;
+		fct_primitive[0] = (t_objprim) {&new_sphere, args1};
+		fct_primitive[1] = (t_objprim) {&new_plane, args0};
+		fct_primitive[2] = (t_objprim) {&new_cylinder, args2};
+		fct_primitive[3] = (t_objprim) {&new_cone, args2};
+		fct_primitive[4] = (t_objprim) {&new_triangle, argsdef};
+		fct_primitive[5] = (t_objprim) {&new_disc, args1};
+		fct_primitive[6] = (t_objprim) {&new_cylinder, args1};
 		++isinit;
 	}
 	return (id < 0 ? NULL : fct_primitive[id]);
 }
 
-void		fill_camera(t_value val, int id, void *obj)
+void		fill_camera(t_value val, int id, ENGINE *engine)
 {
+	t_json		*json;
 	SCENE		*scene;
-	ENGINE		*engine;
 
-	scene = (t_data *)obj->scene;
-	engine = (t_data *)obj->engine;
-	scene->active_camera = new_camera(scene, json_get(val.data.obj, "fov").data.number,
-			json_get(val.data.obj, "znear").data.number,
-			json_get(val.data.obj, "zfar").data.number);
+	scene = engine->scene;
+	json = val.data.obj;
+	scene->active_camera = new_camera(scene, json_get(json, "fov").data.number,
+			json_get(json, "znear").data.number,
+			json_get(json, "zfar").data.number);
+	json = json_get(json, "transform");
 	scene->active_camera->transform = new_rttransform(scene,
-				get_vector3_from_json(json_get(arr.data.obj, "position")),
-				get_vector3_from_json(json_get(arr.data.obj, "rotation")),
-				get_vector3_from_json(json_get(arr.data.obj, "scaling")));
+				get_vector3_from_json(json_get(json, "position")),
+				get_vector3_from_json(json_get(json, "rotation")),
+				get_vector3_from_json(json_get(json, "scaling")));
+	json = json_get(json, "target");
 	scene->active_camera->transform->target = new_rttransform(scene,
-				get_vector3_from_json(json_get(arr.data.obj, "position")),
-				get_vector3_from_json(json_get(arr.data.obj, "rotation")),
-				get_vector3_from_json(json_get(arr.data.obj, "scaling")));
-	scene->bloom_threshold = json_get(arr.data.obj, "bloom_threshold");
-	scene->bloom_intensity = json_get(arr.data.obj, "bloom_intensity");
-	scene->bloom_radius = json_get(arr.data.obj, "bloom_radius");
+				get_vector3_from_json(json_get(json, "position")),
+				vec3_normalize(get_vector3_from_json(json_get(json, "rotation"))),
+				get_vector3_from_json(json_get(json, "scaling")));
+	json = val.data.obj;
+	scene->bloom_threshold = json_get(json, "bloom_threshold");
+	scene->bloom_intensity = json_get(json, "bloom_intensity");
+	scene->bloom_radius = json_get(json, "bloom_radius");
 }
 
-void		fill_map(t_value val, ENGINE *engine, MATERIAL *mtl)
+void		fill_map(t_value val, ENGINE *engine)
 {
+	MATERIAL				*mtl;
 	t_json					*json;
 	void					*fmk;
 
+	mtl = engine.materials;
 	json = arr.data.obj;
 	fmk = engine->framework;
 	mtl->normal_map = load_image_file(fmk, json_get(json, "normal_map").data.s);
@@ -165,20 +207,19 @@ void		fill_primitive(t_value val, int id, void *obj)
 		arr = json_get(val.data.obj, "transform");
 		p->transform = new_rttransform(scene,
 				get_vec3_json(json_get(arr.data.obj, "position")),
-				get_vec3_json(json_get(arr.data.obj, "rotation")),
+				vec3_normalize(get_vec3_json(json_get(arr.data.obj, "rotation"))),
 				get_vec3_json(json_get(arr.data.obj, "scaling")));
-		//fct = fct_mat(key_mat(djb2(json_get(val.data.obj, "material").data.s)));
-		//if (fct != NULL)
 		p->material = get_mtl_by_name(scene, json_get(val.data.obj, "material").data.s);
 	}
 }
 
-void		create_engine(t_value engin)
+ENGINE		*create_engine(t_value engin)
 {
 }
 
-void		create_scene(char *file, ENGINE *engine)
+void		create_scene(t_value val, ENGINE *engine)
 {
+	t_value		scene;
 	t_value		camera;
 	t_value		materials;
 	t_value		primitives;
