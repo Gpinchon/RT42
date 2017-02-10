2cf6df47e63fef906cd26a0436f4b9baa3b48419
Guillaume Pinchon <gpinchon@debian>
2017-02-10 23:48:30 +0100

    42norme corrected HDR

--- src/blur.c
+++ src/blur.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   blur.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarbari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gpinchon <gpinchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/10 11:37:24 by mbarbari          #+#    #+#             */
-/*   Updated: 2017/02/10 11:58:18 by mbarbari         ###   ########.fr       */
+/*   Updated: 2017/02/10 22:56:48 by gpinchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ VEC4		blur_sample(ENGINE *engine, t_point2 c, float intensity)
 VEC4		blur_sample_with_threshold(ENGINE *engine, t_point2 p,
 	float intensity, float threshold)
 {
-	VEC4		c;
+	VEC3		c;
 	VEC4		vcolor;
 	VEC2		uv;
 	t_point2	size;
@@ -57,9 +57,9 @@ VEC4		blur_sample_with_threshold(ENGINE *engine, t_point2 p,
 			intensity) * size.x, (uv.y + (engine->poisson_disc[i].y * 2 - 1)
 			* intensity) * size.y};
 		p = (t_point2){CLAMP(p.x, 0, size.x - 1), CLAMP(p.y, 0, size.y - 1)};
-		c = *((VEC4*)get_buffer_value(engine->hdrbuffer, p));
-		if (vec4_dot(c, new_vec4(0.299, 0.587, 0.114, 1)) > threshold)
-			vcolor = vec4_add(vcolor, vec4_saturate(c));
+		c = *((VEC3*)get_buffer_value(engine->hdrbuffer, p));
+		if (vec3_dot(c, new_vec3(0.2125, 0.7154, 0.0721)) > threshold)
+			vcolor = vec4_add(vcolor, vec4_saturate(vec3_to_vec4(c, 1)));
 		i++;
 	}
 	return (vec4_fdiv(vcolor, 64));
